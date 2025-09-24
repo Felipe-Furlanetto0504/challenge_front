5685c3a5c328eed93e9cb93dabc83575bfe46c63
Felipe-Furlanetto0504 <felipe.furlanetto.4@gmail.com>
2025-09-24 13:48:10 -0300

    criada a pasta img e adicionado as imagens e adicionado o icon

--- challenge/src/components/Cabecalho/Cabecalho.tsx
+++ challenge/src/components/Cabecalho/Cabecalho.tsx
@@ -3,7 +3,6 @@ import Menu from "../Menu/Menu";
 export default function Cabecalho(){
     return(
         <header>
-            <h1>Menu de Navegação</h1>
             <Menu/>
         </header>
     );
