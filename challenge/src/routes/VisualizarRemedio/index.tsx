cd0a9f878ee87e9ff22a452eb19f86e79d1036b4
Buenozw <jvictorcastelini2006@gmail.com>
2025-09-29 11:53:29 -0300

    chamando as informações as informações da tabela ao clicar no ver

--- challenge/src/routes/VisualizarRemedio/index.tsx
+++ challenge/src/routes/VisualizarRemedio/index.tsx
@@ -3,36 +3,41 @@ import { useParams } from "react-router-dom";
 import type { Tiporemedio } from "../../types/tipoRemedio";
 import Informacoes from "../../components/Informacoes/Informacoes";
 
-export default function VisualizarRemedios(){
-    useEffect(() => {
+export default function VisualizarRemedios() {
+  useEffect(() => {
     document.title = "Ver Remédios";
   }, []);
 
+  const { id } = useParams();
+  const [remedio, setRemedios] = useState<Tiporemedio>();
 
-  const {id} = useParams();
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await fetch(`http://localhost:3000/remedios/${id}`);
+        if (!response.ok) throw new Error("Erro na API");
+        const data: Tiporemedio = await response.json();
 
-  const[remedio, setRemedios] = useState<Tiporemedio>();
-
-  useEffect(()=>{
-  
-    (async()=>{
-      const response = fetch(`http://localhost:3000/remedios/`);
-      const data: Tiporemedio[] = await (await response).json();
-      const achaproduto: Tiporemedio | undefined = data.find(a => a.id === Number (id));
-      setRemedios(achaproduto);
-
+        setRemedios(data);
+      } catch (error) {
+        console.error("Erro ao buscar o remédio:", error);
+      }
     })();
-         
-  },[id]);
+  }, [id]);
 
-  return(
+  return (
     <main>
-        <h1>Ver Remédios</h1>
-        {remedio ? <Informacoes idprops={remedio.id}nomeprops={remedio.nome} precoprops={remedio.preco} descricaoprops={remedio.descricao}/> : <p>Não tem esse remedio disponivel</p>}
+      <h1>Ver Remédios</h1>
+      {remedio ? (
+        <Informacoes
+          idprops={remedio.id}
+          nomeprops={remedio.nome}
+          precoprops={remedio.preco}
+          descricaoprops={remedio.descricao}
+        />
+      ) : (
+        <p>Não tem esse remédio disponível</p>
+      )}
     </main>
-
   );
-
-  
-
 }
