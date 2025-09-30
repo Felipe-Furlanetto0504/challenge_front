9d046551f61088afcff50909428dd9ca047f2430
joaocaetano1310 <joaovictorcaetano1310@gmail.com>
2025-09-30 20:18:12 -0300

    pagina contato pronta

--- challenge/src/routes/Contato/index.tsx
+++ challenge/src/routes/Contato/index.tsx
@@ -9,23 +9,40 @@ export default function Contato() {
   return (
     <main>
 
-      <h1>Contato</h1>
+      <h1 className="bg-[#e6f9fc] text-3xl space-y-4 p-4 text-cyan-700">Contato</h1>
 
-      <section>
-        <h2>Endereço:</h2>
-        <div>
-        <p>
-          Av. Dr. Enéas de Carvalho Aguiar, 255 Cerqueira César 05403-000 São
-          Paulo - Brasil Tel.: (011) 2661-0000
-        </p>
+     <section className="p-4 bg-[#e6f9fc] space-y-4 h-[64vh]">
+  
+  <div>
+    <h2 className="font-semibold italic text-gray-800">Endereço:</h2>
+    <div className="bg-[#ffffff] border border-gray-400 rounded-md p-3 mt-1">
+      <p className="text-gray-800">
+        Av. Dr. Enéas de Carvalho Aguiar, 255 Cerqueira César 05403-000 São
+        Paulo - Brasil Tel.: (011) 2661-0000
+      </p>
+    </div>
+  </div>
 
-        <h2>E-mail:</h2>
-        <p>webmaster.nci@hc.fm.usp.br</p>
+  
+  <div>
+    <h2 className="font-semibold italic text-gray-800">E-mail:</h2>
+    <div className="bg-[#ffffff] border border-gray-400 rounded-md p-3 mt-1">
+      <p className="text-gray-800">webmaster.nci@hc.fm.usp.br</p>
+    </div>
+  </div>
 
-        <h2>Site Oficial:</h2>
-        <a href="http://www.hc.fm.usp.br">Portal Ofical do HC</a>
-        </div>
-      </section>
+
+  <div>
+    <h2 className="font-semibold italic text-gray-800">Site Oficial:</h2>
+    <div className="bg-[#ffffff] border border-gray-400 rounded-md p-3 mt-1">
+      <a href="http://www.hc.fm.usp.br" 
+         className="text-blue-600 hover:underline">
+         Portal Oficial do HC
+      </a>
+    </div>
+  </div>
+</section>
+
 
     </main>
   );
