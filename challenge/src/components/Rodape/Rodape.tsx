a80d8a4eab9689ae26bb9b50080f4c789c646332
Buenozw <jvictorcastelini2006@gmail.com>
2025-09-30 19:22:02 -0300

    footer com css feito

--- challenge/src/components/Rodape/Rodape.tsx
+++ challenge/src/components/Rodape/Rodape.tsx
@@ -3,7 +3,7 @@ export default function Rodape() {
   return (
     <footer>
       <p>&copy; Todos os direitos reservados - 2025. Site HC</p>
-      <a href="https://www.instagram.com/hospitalhcfmusp">Pagína Do instagram</a>
+      <a href="https://www.instagram.com/hospitalhcfmusp">-Pagína Do instagram</a>
     </footer>
   );
 
